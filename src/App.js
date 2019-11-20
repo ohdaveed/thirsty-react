2d4f837cdc44e0904a1d29408bda0e246a3209ef
David Arrizon <arrizon.david@gmail.com>
2019-11-20 17:26:42 -0600

    created login form

--- src/App.js
+++ src/App.js
@@ -1,26 +1,93 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React from "react";
+import "./App.css";
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+import LoginRegisterForm from "./LoginRegisterForm";
+
+class App extends React.Component {
+	constructor() {
+		super();
+
+		this.state = {
+			loggedIn: false,
+			loggedInUsername: null // you might want to track some info here
+			// helpful for "logged in as reuben@reuben.com"
+		};
+	}
+
+	login = async (loginInfo) => {
+		const response = await fetch(
+			process.env.REACT_APP_API_URL + "/api/v1/users/login",
+			{
+				method: "POST",
+				// in the express unit, the browser would automatically send the cookie
+				// with each request (GET and POST), but fetch will not.
+				// adding this line will make fetch send the cookie
+				// if you forget this you will not be logged in for that request
+				credentials: "include",
+				body: JSON.stringify(loginInfo),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		);
+
+		console.log(response);
+
+		const parsedLoginResponse = await response.json();
+
+		// want to change the screen in the app when we know the login was good
+		// and show dog container
+		if (parsedLoginResponse.status.code === 200) {
+			this.setState({
+				loggedIn: true,
+				loggedInEmail: parsedLoginResponse.data.email
+			});
+		} else {
+			// hungry for more/in your project -- show a message to the user, or maybe
+			// show the message from the response to the user,
+			// clear the login form, if snazzy, highlight in red?
+			// so they can try again
+			console.log("Login Failed:");
+			console.log(parsedLoginResponse);
+		}
+	};
+
+	register = async (registerInfo) => {
+		const response = await fetch(
+			process.env.REACT_APP_API_URL + "/api/v1/users/register",
+			{
+				method: "POST",
+				credentials: "include",
+				body: JSON.stringify(registerInfo),
+				headers: {
+					"Content-Type": "application/json"
+				}
+			}
+		);
+
+		const parsedRegisterResponse = await response.json();
+
+		if (parsedRegisterResponse.status.code === 201) {
+			this.setState({
+				loggedIn: true,
+				loggedInEmail: parsedRegisterResponse.data.email
+			});
+		} else {
+			console.log("Register Failed:");
+			console.log(parsedRegisterResponse);
+		}
+	};
+
+	render() {
+		return (
+			<div className="App">
+				<LoginRegisterForm
+					login={this.login}
+					register={this.register}
+				/>
+			</div>
+		);
+	}
 }
 
 export default App;
